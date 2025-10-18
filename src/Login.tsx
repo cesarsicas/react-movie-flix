a5089b07b2dc64624c583e6a65c0e3bb6b3cc91e
cesarsicas <jctsiqueira@gmail.com>
2025-10-18 09:17:08 -0300

    [chore] Included react router

---
+++ src/Login.tsx
@@ -0,0 +1,5 @@
+export function Login(){
+    return (
+        <p>Just a test bro</p>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Login } from './Login.tsx';
+
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>,
+);
