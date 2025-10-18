f616a982f7ed68e82068ec374477568fe6131101
cesarsicas <jctsiqueira@gmail.com>
2025-10-18 14:28:27 -0300

    [fix] fix movieList behavior

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -21,7 +21,7 @@ const MoviesList: React.FC<React.PropsWithChildren<{}>> = () => {
             <h2 className="text-2xl font-bold mb-6 text-gray-800">Popular Movies</h2>
             
             {/* 💡 THE RESPONSIVE FIX IS HERE: */}
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
                 {
                     dummyMovies.map((movie) => {
                         return (
