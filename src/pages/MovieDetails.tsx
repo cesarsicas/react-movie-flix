96674a878fce049170ffd837cad77d0ece711ced
cesarsicas <jctsiqueira@gmail.com>
2025-10-24 09:02:57 -0300

    feat - improve movie details

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -32,18 +32,45 @@ export function MovieDetails() {
           </div>
         </div>
       </Banner>
-      <h2>Synopsis</h2>
-      <p>
-        An elite special forces operative must infiltrate a terrorist
-        organization to prevent a global catastrophe. With time running out,
-        every decision could mean the difference between salvation and
-        destruction.
-      </p>
-      <h2>Cast and Crew</h2>
-      <h3>Director</h3>
-      <p>Michael Chen</p>
-      <h3>Cast</h3>
-      <p>John Harrison, Sarah Mitchell, David Rodriguez</p>
+      <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-[2fr_1fr]">
+        <div className="mt-6">
+          <h2 className="text-2xl font-bold text-gray-800">Sinopsis</h2>
+          <p className="mb-6">
+            An elite special forces operative must infiltrate a terrorist
+            organization to prevent a global catastrophe. With time running out,
+            every decision could mean the difference between salvation and
+            destruction.
+          </p>
+          <h2 className="mb-6 text-2xl font-bold text-gray-800">
+            Cast and Crew
+          </h2>
+          <h3>Director</h3>
+          <p>Michael Chen</p>
+          <h3>Cast</h3>
+          <p>John Harrison, Sarah Mitchell, David Rodriguez</p>
+        </div>
+
+        <div className="mt-6 rounded-md border-2 border-solid border-gray-300 p-4">
+          <h2 className="text mb-4 font-bold text-gray-800"> Movie details</h2>
+
+          <div className="mb-2 flex w-full justify-between">
+            <p>Genre</p>
+            <p>Action</p>
+          </div>
+          <div className="mb-2 flex w-full justify-between">
+            <p>Year</p>
+            <p>2005</p>
+          </div>
+          <div className="mb-2 flex w-full justify-between">
+            <p>Duration</p>
+            <p>2h 15min</p>
+          </div>
+          <div className="flex w-full justify-between">
+            <p>Rating</p>
+            <p>4/5</p>
+          </div>
+        </div>
+      </div>
     </PageContainer>
   );
 }
