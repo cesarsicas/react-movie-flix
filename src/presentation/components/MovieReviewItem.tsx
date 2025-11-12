e5c489c0ba9cd9c2f38f52c5114533f724753a84
cesarsicas <jctsiqueira@gmail.com>
2025-11-12 18:35:56 -0300

    feat - replace comment image with UseAvatar

--- src/presentation/components/MovieReviewItem.tsx
+++ src/presentation/components/MovieReviewItem.tsx
@@ -1,4 +1,5 @@
 import type ReviewModel from "../../domain/model/ReviewModel";
+import { UserAvatar } from "./UserAvatar";
 
 const MoviewReviewItem: React.FC<
   React.PropsWithChildren<{ movie: ReviewModel }>
@@ -6,7 +7,7 @@ const MoviewReviewItem: React.FC<
   return (
     <div className="mb-4 rounded-md border-1 border-solid border-gray-300 p-4">
       <div className="mb-2 flex items-center">
-        <span className="h-[30px] w-[30px] rounded-[15px] bg-gray-900" />
+        <UserAvatar />
         <div className="mx-2">
           <p>{props.movie.userName}</p>
           <p className="text-xs text-gray-500">2024-09-14</p>
--- src/presentation/components/UserAvatar.tsx
+++ src/presentation/components/UserAvatar.tsx
@@ -2,7 +2,7 @@ import movieTheater from "../../assets/poster_placeholder.png";
 
 export function UserAvatar() {
   return (
-    <div className="ml-2 h-[35px] w-[35px] rounded-[50%] border-[1px] border-gray-900 p-1">
+    <div className="h-[35px] w-[35px] rounded-[50%] border-[1px] border-gray-900 p-1">
       <img src={movieTheater} alt="" />
     </div>
   );
